ae3d297126c82901969a9b3af3c75915a74db5bd
FRA082 <francois.starck@mil.be>
2021-02-16 17:22:34 +0100

    Updated prj ready to start

---
+++ src/pages/Home.js
@@ -0,0 +1,15 @@
+//Plus nécessaire depuis la dernière MaJ
+import React from 'react';
+
+//
+const Home = () => {
+    return(
+        //Max un seul html balise
+        //Pêt un fragment <>...</> 
+        <div className="home">
+            <h1>Acceuil</h1>
+        </div>
+    )
+}
+
+export default Home;
